1217aac12fd6dccadb67a01ea5247d24fa3142e1
Abhijit Kumar <abhijeetmitian1@gmail.com>
2023-01-17 04:04:40 +0530

    change in Upload,Location ,Offline save
    ChangeLog:
    
    1) Solve Firebase uploading problem
    
    -> Solved Firebase uploading problem by correctly positioning the Intent (image data) and changing the storage permission of Firebase to true.
    
    2) Displaying Location
    
    -> Implemented location display by taking the latitude and longitude and using the inbuilt Geocoder Reverse geo-coding to fetch the location.
    
    3) Added a function to check if data and wifi is enabled or not.
    
    -> Added a function to check if data and wifi are enabled or not by using the ConnectivityManager class to check network info (it returns a boolean value).
    
    4) If Internet is not connected then save data to Internal storage.
    
    -> If internet is not connected, data is saved to Internal storage by building a function saveImageOffline which takes a bitmap as an input. Using Outstream and ContentValues, the image is stored in JPG format by creating a specific folder "snapmap/img/" using the name 'Img_<systemTimeinMilliSec>.jpg'. Additionally, the ACCESS_MEDIA_LOCATION permission was added in the manifest file and requested from the user at runtime for storage permission.
    
    Co-Authored-By: Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
    Co-Authored-By: verma321 <92589137+verma321@users.noreply.github.com>
    Co-Authored-By: UTKARSHANANDX <75621246+UTKARSHANANDX@users.noreply.github.com>

--- snapmap/app/src/main/java/com/example/snapmap/MainActivity.java
+++ snapmap/app/src/main/java/com/example/snapmap/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.snapmap;
 
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -10,21 +9,28 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,9 +38,16 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.TimeZone;
 
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -48,11 +61,12 @@ public class MainActivity extends AppCompatActivity {
 
     private final int CAMERA_REQ_CODE = 100;
     private final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private FirebaseStorage storage;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
     private StorageReference storagereference;
-    public Uri imageUri;
     ImageView imgCamera,mapView;
     TextView latLongTextView;
+    Intent imgdata;
+    double latitude,longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         imgCamera = findViewById(R.id.imgCamera);
         Button btnCamera = findViewById(R.id.btnCamera);
+
+        storagereference = FirebaseStorage.getInstance().getReference();
 
 
         //ask for location permission
@@ -82,9 +98,7 @@ public class MainActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Call the uploadImageToFirebase() method here
-//                uploadImageToFirebase(img);
-                Toast.makeText(MainActivity.this, "Uploaded successfully", Toast.LENGTH_SHORT).show();
+                onCaptureImage(imgdata);
             }
         });
 
@@ -95,39 +109,61 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == CAMERA_REQ_CODE) {
-                double latitude,longitude;
-                assert data != null;
+                assert data != null : "The image data is NULL";
+
                 Bitmap img = (Bitmap) (data.getExtras().get("data"));
-//                Uri imageUri = data.getData();
+
                 Location location = getCurrentLocation();
-                assert location != null;
-                latitude=location.getLatitude();
-                longitude=location.getLongitude();
-//                addLocationToImage(img, latitude, longitude);
+                assert location != null : "Location is NULL";
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
 
 
                 String bingMapsApiKey = "AmqwMGmPtRT0bZK_I2ww-yIDuL2WYd3YsWhjbSxYMWMqq-IJtE0ifNWMvJPJPz2S"; // Replace with your own Bing Maps API key
-                String center = latitude+","+longitude; // Coordinates for the center of the map view
+                String center = latitude + "," + longitude; // Coordinates for the center of the map view
                 int zoom = 16; // Zoom level for the map view
                 int width = 800; // Width of the map image in pixels
                 int height = 560; // Height of the map image in pixels
 
-// Construct the URL for the static map image
-                String mapUrl = "https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/" + center + "/" + zoom + "?mapSize=" + width + "," + height +"&pushpin="+center+"&key=" + bingMapsApiKey;
+                // Construct the URL for the static map image
+                String mapUrl = "https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/" + center + "/" + zoom + "?mapSize=" + width + "," + height + "&pushpin=" + center + "&key=" + bingMapsApiKey;
+
+                //calling getLocationName() to get the name of location
+                String locationName = getlocationName(latitude, longitude);
 
                 // Set the latitude and longitude text
-                latLongTextView.setText(String.format("Latitude: %s, Longitude: %s", latitude, longitude));
+                latLongTextView.setText(String.format("Location: %s\nLatitude: %s, Longitude: %s", locationName, latitude, longitude));
 
                 // Use Picasso library to load the map image
                 Picasso.get().load(mapUrl).into(mapView);
 
                 imgCamera.setImageBitmap(img);
-//                onCaptureImage(data);
+                imgdata = data;
+
+                //if data enabled then upload otherwise save offline
+                if (!isDataEnabled()) {
+                    //Ask to access storage permission
+                    requestStoragePermission();
+                    // Store the image locally
+                    saveImageOffline(img);
+                    Toast.makeText(this, "Saving offline...", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
 
+    //Asking for Location permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION}, REQUEST_STORAGE_PERMISSION);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+            }
+        }
+    }
 
+    //asking the user for location permission
     private void askForLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -154,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             // Permission has already been granted
-//            Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+
         }
     }
     @Override
@@ -170,6 +206,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //getting location from android LocationManager class
     private Location getCurrentLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -203,25 +240,33 @@ public class MainActivity extends AppCompatActivity {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         img.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         byte[] bb = bytes.toByteArray();
-//        imgCamera.setImageBitmap(img);
 
-        uploadToFirebase(bb);
+        uploadToFirebase(bb,"Latitude : "+latitude+" ,Longitude : "+longitude);
     }
 
-    private void uploadToFirebase(byte[] bb) {
+
+
+    private void uploadToFirebase(byte[] bb,String locStr)
+    {
         String filename = "IMG";
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-DD_HH:MM:SS", Locale.CANADA);
-            Date now = new Date();
-            filename = formatter.format(now);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+            filename = dateFormat.format(new Date());
+
         }
 
         ProgressDialog dialog = new ProgressDialog(this);
         dialog.setTitle("Uploading File...");
         dialog.show();
 
-
         StorageReference riversRef = storagereference.child("images/"+filename+".jpg");
+        StorageReference locRef = storagereference.child("Location_Info/"+filename+".txt");
+
+        byte[] textBytes = locStr.getBytes();
+
+        locRef.putBytes(textBytes);
+
         riversRef.putBytes(bb).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -248,6 +293,64 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    //function for reverseGeocoding using geocoordinates.
+    private String getlocationName(double latitude,double longitude)
+    {
+        String cityName = null;
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    if(address.getLocality() != null){
+                        cityName = address.getLocality();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cityName;
+    }
+    //Function to check weather internet is on or off
+    private boolean isDataEnabled() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE || networkInfo.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    //this method will save the image (if internet is off) in folder snapmap/img/ with name as Image_<systemtimeinmillisec>.jpg
+    private void saveImageOffline(Bitmap bitmap){
+
+        OutputStream fos;
+
+        try{
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+
+                ContentResolver resolver = getContentResolver();
+                ContentValues contentValues =  new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_" + System.currentTimeMillis() + ".jpg");
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "snapmap/img/");
+                Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                Objects.requireNonNull(fos);
+
+                Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch(Exception e){
+
+            Toast.makeText(this, "Image not saved \n" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
 
 
